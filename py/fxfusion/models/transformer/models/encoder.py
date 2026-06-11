3f999b9dc1c81adce0bacf42be21f753fe7168ad
magnimel <65434087+magnimel@users.noreply.github.com>
2026-06-11 04:00:19 -0700

    feat: implement Encoder class for transformer model

---
+++ py/fxfusion/models/transformer/models/encoder.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from layers.embedding import InputEmbedding, PositionalEncoding
+from blocks.encoder_block import EncoderBlock
+
+class Encoder(nn.Module):
+    def __init__(self, d_model: int, h: int, vocab_size: int, expansion_factor: int, dropout: float = 0.1, Nx: int = 1):
+        super().__init__()
+        assert Nx > 0
+        self.norm = nn.LayerNorm(d_model)
+        self.in_embedding = InputEmbedding(vocab_size, d_model)
+        self.pos_encoding = PositionalEncoding(d_model, dropout=dropout)
+        self.encoder_blocks = nn.ModuleList([
+            EncoderBlock(d_model, h, expansion_factor, dropout)
+            for _ in range(Nx)
+        ])
+        
+    def forward(self, x, mask=None):
+        x = self.in_embedding(x)
+        x = self.pos_encoding(x)
+
+        for block in self.encoder_blocks:
+            x = block(x, mask)
+
+        return self.norm(x)
