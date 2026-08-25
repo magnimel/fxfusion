aeae654df37c8dd4e5093fd2dd09dc7251f05df5
magnimel <65434087+magnimel@users.noreply.github.com>
2026-08-25 02:58:54 -0700

    feat: add GraphContext and CacheBuilderFn abstractions

--- csrc/include/runtime/runtime_types.hpp
+++ csrc/include/runtime/runtime_types.hpp
@@ -17,6 +17,11 @@ struct Cache {
     virtual ~Cache() = default;
 };
 
+struct GraphContext {
+    virtual ~GraphContext() = default;
+    virtual void ensure_workspace() {} 
+};
+
 using KernelFn = void (*)(
     TensorRegistry&,
     const TensorIds&,
@@ -24,5 +29,14 @@ using KernelFn = void (*)(
     const Params&,
     const Cache*
 );
+
+using CacheBuilderFn = std::unique_ptr<Cache> (*)(
+    GraphContext* ctx,
+    TensorRegistry& reg,
+    const TensorIds& input_ids,
+    const TensorIds& output_ids,
+    const Params& params
+);
+
 
 }
